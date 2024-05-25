62f3c355c37c07adb0980925b866c32df434426e
makano <makanobush@gmail.com>
2024-05-25 21:22:45 +0300

    Displays github url

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 import { Button } from "@/components/button";
 import { Card } from "@/components/card";
 import LocalStore from "@/data/local";
-import { Component, InputWrapper, Text, Widget } from "rayous";
-import { asyncComponent, buildProps } from "rayous/extra";
+import { Component, InputWrapper, List, Text, Widget } from "rayous";
+import { EntryController, asyncComponent, buildProps } from "rayous/extra";
 import { React } from "rayous/react";
 
 @asyncComponent()
@@ -14,19 +14,26 @@ export default class extends Component {
 	async build(props: buildProps) {
 		const github_url = LocalStore.get('github_url');
 		if(github_url) {
+			const anime_list = await fetch(github_url).then(r => r.json());
 			return new Widget({
 				children: [
-
+					new List({
+						items: anime_list,
+						template(item: { name: string }){
+							return <Text>{item.name}</Text>
+						}
+					})
 				]
 			});
 		} else {
+			const git_url = new EntryController('');
 			return new Widget({
 				children: [
 					<Card class="m-[10px] w-[320px] bg-base-200"
 					title={<Text>Github URL</Text>}
 					content={[
 						<InputWrapper
-						id="git_url"
+						controller={git_url}
 						title="URL"
 						class="input input-bordered mt-5"></InputWrapper>,
 						<Text class="mt-4">Your github url where you put your anime list</Text>
@@ -35,7 +42,7 @@ export default class extends Component {
 					actions={[
 						<Button
 						onClick={() => {
-							LocalStore.set('github_url', (this._currentWidget.find('#git_url') as any).val())
+							LocalStore.set('github_url', git_url.get())
 						}}
 						variant="accent">Set</Button>
 					]}></Card>
